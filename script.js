97f30434d058258e10cc47afda8f2e6b516f4f7c
ggaspar22 <andrijchornobajj@gmail.com>
2021-12-06 10:26:21 +0200

    added hw 12

---
+++ script.js
@@ -0,0 +1,66 @@
+//  ------------------------ завдання з футболками ------------------------ 
+function photo_1(){
+    let a = prompt('Write your link');
+    document.querySelector('.box1 img').src = a;
+}
+function photo_2(){
+    let b = prompt('Write your link');
+    document.querySelector('.box2 img').src = b;
+}
+function photo_3(){
+    let c = prompt('Write your link');
+    document.querySelector('.box3 img').src = c;
+}
+
+//  ------------------------ завдання з футболками ------------------------ 
+//  ------------------------ завдання із зміною кольору блоку  ------------------------ 
+
+
+// let box = document.getElementById('box');
+// console.log(box);
+
+// let x = ['yellow','green','red']
+// let a = 0;
+// box.onmouseenter = function(){
+//    if(a%2){
+//     this.style.background = 'yellow';
+//    }else if(a%3){
+//     this.style.background = 'green';
+//    }else if (a!=a%2 && a!=a%3){
+//     this.style.background = 'red';
+//    }
+//    a++;
+// }
+
+// box.onmouseleave = function(){
+//     this.style.background = 'blueviolet';
+// }
+//  ------------------------ завдання із зміною кольору блоку  ------------------------ 
+
+//  ------------------------ завдання із секретом  ------------------------ 
+// let box = document.getElementById('box');
+// box.onmouseleave = function(){
+//     this.style.background = 'blueviolet';
+//     this.textContent = null;
+// }
+// box.onmouseenter = function(){
+//     this.style.background = 'red';
+//     this.style.textAlign = 'center';
+//     this.style.fontSize = '40px';
+//     this.textContent = 'Hello I have a secret';
+// }
+// box.onmousedown = function(){
+//     this.textContent = 'I dont say you ';
+//     this.style.background = 'yellow';
+// }
+// box.onmouseup = function(){
+//     this.style.background = 'red';
+//     this.style.textAlign = 'center';
+//     this.style.fontSize = '40px';
+//     this.textContent = 'Hello I have a secret';
+// }
+
+//  ------------------------ завдання із секретом  ------------------------ 
+
+
+
